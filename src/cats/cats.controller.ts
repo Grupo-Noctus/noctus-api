d1e04456c4d88f4962ff4ff1c77648749b6d6451
GabrielaCamilo <gabrielacamilovargasss@gmail.com>
2025-03-13 20:26:32 -0300

    chore: cats swagger config

---
+++ src/cats/cats.controller.ts
@@ -0,0 +1,38 @@
+import { Body, Controller, Get, Param, Post } from '@nestjs/common';
+import {
+  ApiBearerAuth,
+  ApiOperation,
+  ApiResponse,
+  ApiTags,
+  ApiBody,
+} from '@nestjs/swagger';
+import { CatsService } from './cats.service';
+import { CreateCatDto } from './dto/create-cat.dto';
+import { Cat } from './entities/cat.entity';
+
+
+@ApiBearerAuth()
+@ApiTags('cats')
+@Controller('cats')
+export class CatsController {
+  constructor(private readonly catsService: CatsService) {}
+
+  @Post()
+  @ApiOperation({ summary: 'Create a cat' })
+  @ApiResponse({ status: 403, description: 'Forbidden.' })
+  @ApiBody({ type: CreateCatDto })
+  async create(@Body() createCatDto: CreateCatDto): Promise<Cat> {
+    return this.catsService.create(createCatDto);
+  }
+
+  @Get(':id')
+  @ApiOperation({summary: ' Get cats by ID'})
+  @ApiResponse({
+    status: 200,
+    description: 'The found record',
+    type: Cat,
+  })
+  findOne(@Param('id') id: string): Cat {
+    return this.catsService.findOne(+id);
+  }
+}
